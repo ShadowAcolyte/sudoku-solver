6a6e315a840425da71e64657aac99a574eb08f74
Samvrit <samvrit8703@gmail.com>
2021-08-16 14:15:17 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@
 
 //test key: 004962300060100400820370006001406020002750000093200704270030940100000275900820001
 
-using std::cout, std::endl, std::string, std::bitset;
+using std::string;
 using std::chrono::duration_cast, std::chrono::milliseconds, std::chrono::system_clock;
 
-void display(const std::string& key)
+void display(const string& key)
 {
     for (int i = 0; i < 81; i ++) {
         if (i % 9 == 0)
@@ -24,7 +24,7 @@ void display(const std::string& key)
     std::cout << "\n+ - + - + - + - + - + - + - + - + - +\n";
 }
 
-void display(const std::string& key, const std::string& solvedkey)
+void display(const string& key, const string& solvedkey)
 {
     for (int i = 0; i < 81; i ++) {
         if (i % 9 == 0)
@@ -41,26 +41,26 @@ void display(const std::string& key, const std::string& solvedkey)
 
 int main(int argc, char** argv)
 {
-    cout << "\nSudoku Solver " << dye::light_green("[Version 1.0.0]\n");
-    cout << "Author: " << dye::aqua("ShadowAcolyte\n");
-    cout << dye::aqua("https://github.com/ShadowAcolyte\n\n");
+    std::cout << "\nSudoku Solver " << dye::light_green("[Version 1.0.0]\n");
+    std::cout << "Author: " << dye::aqua("ShadowAcolyte\n");
+    std::cout << dye::aqua("https://github.com/ShadowAcolyte\n\n");
 
     if (argc == 1) {
-        cout << dye::light_red("ERROR: No key specified!\n");
+        std::cout << dye::light_red("ERROR: No key specified!\n");
         return -1;
     }
     string key(argv[1]);
     if (key.length() != 81) {
-        cout << dye::light_red("ERROR: Key length must be 81 digits!\n");
+        std::cout << dye::light_red("ERROR: Key length must be 81 digits!\n");
         return -1;
     }
 
-    if (key.find_first_not_of("0123456789.") != std::string::npos) {
-        cout << dye::light_red("ERROR: Key can only contain numbers and dots('.')\n");
+    if (key.find_first_not_of("0123456789.") != string::npos) {
+        std::cout << dye::light_red("ERROR: Key can only contain numbers and dots('.')\n");
         return -1;
     }
 
-    cout << "Original Board: \n";
+    std::cout << "Original Board: \n";
     display(key);
 
     uint64_t t_start, t_end;
@@ -69,7 +69,7 @@ int main(int argc, char** argv)
     solver.solve();
     t_end = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 
-    cout << "\nSolved Board: \n";
+    std::cout << "\nSolved Board: \n";
     display(key, solver.getstate());
-    cout << "Time taken: " << t_end - t_start << "ms" << endl;
+    std::cout << "Time taken: " << t_end - t_start << "ms" << std::endl;
 }
