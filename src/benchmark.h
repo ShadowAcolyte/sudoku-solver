ed20d243b1dea5f653b23dded05b4e7597df146a
Samvrit <samvrit8703@gmail.com>
2021-09-01 23:14:53 +0530

    Added benchmark tool

---
+++ src/benchmark.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <cstdint>
+#include <chrono>
+
+class benchmark {
+    uint64_t t_start, t_end;
+public:
+    benchmark() {
+        t_start = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    }
+    inline void reset() {
+        t_end = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        std::cout << "\nTime taken : " << (t_end - t_start) / 1000.0f << "ms\n";
+        t_start = t_end;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
-#include <chrono>
 #include <bitset>
 #include "color.hpp"
 #include "solver.h"
+#include "benchmark.h"
 
 //test key: 004962300060100400820370006001406020002750000093200704270030940100000275900820001
 
-using std::string;
-using std::chrono::duration_cast, std::chrono::milliseconds, std::chrono::system_clock;
-
-void display(const string& key)
+void display(const std::string& key)
 {
     for (int i = 0; i < 81; i ++) {
         if (i % 9 == 0)
-            std::cout << "\n+ - + - + - + - + - + - + - + - + - +\n|";
+            std::cout << "\n+---+---+---+---+---+---+---+---+---+\n|";
         std::cout << " " << hue::light_purple;;
         if (key.at(i) == '0' || key.at(i) == '.')
             std::cout << " ";
@@ -21,14 +18,14 @@ void display(const string& key)
             std::cout << key.at(i);
         std::cout << hue::reset << " |";;
     }
-    std::cout << "\n+ - + - + - + - + - + - + - + - + - +\n";
+    std::cout << "\n+---+---+---+---+---+---+---+---+---+\n";
 }
 
-void display(const string& key, const string& solvedkey)
+void display(const std::string& key, const std::string& solvedkey)
 {
     for (int i = 0; i < 81; i ++) {
         if (i % 9 == 0)
-            std::cout << "\n+ - + - + - + - + - + - + - + - + - +\n|";
+            std::cout << "\n+---+---+---+---+---+---+---+---+---+\n|";
         std::cout << " ";
         if (key.at(i) == '0' || key.at(i) == '.')
             std::cout << hue::light_green << solvedkey.at(i);
@@ -36,7 +33,7 @@ void display(const string& key, const string& solvedkey)
             std::cout << hue::light_purple << key.at(i);
         std::cout << hue::reset << " |";;
     }
-    std::cout << "\n+ - + - + - + - + - + - + - + - + - +\n";
+    std::cout << "\n+---+---+---+---+---+---+---+---+---+\n";
 }
 
 int main(int argc, char** argv)
@@ -49,13 +46,13 @@ int main(int argc, char** argv)
         std::cout << dye::light_red("ERROR: No key specified!\n");
         return -1;
     }
-    string key(argv[1]);
+    std::string key(argv[1]);
     if (key.length() != 81) {
         std::cout << dye::light_red("ERROR: Key length must be 81 digits!\n");
         return -1;
     }
 
-    if (key.find_first_not_of("0123456789.") != string::npos) {
+    if (key.find_first_not_of("0123456789.") != std::string::npos) {
         std::cout << dye::light_red("ERROR: Key can only contain numbers and dots('.')\n");
         return -1;
     }
@@ -63,13 +60,11 @@ int main(int argc, char** argv)
     std::cout << "Original Board: \n";
     display(key);
 
-    uint64_t t_start, t_end;
-    t_start = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    benchmark b;
     Solver solver{key};
     solver.solve();
-    t_end = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    b.reset();
 
     std::cout << "\nSolved Board: \n";
     display(key, solver.getstate());
-    std::cout << "Time taken: " << t_end - t_start << "ms" << std::endl;
 }
